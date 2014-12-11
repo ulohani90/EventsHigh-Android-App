d67a1fa5d178fb4673e5b6b3444dab0ea4991481
arvind batra <arvindbatra@gmail.com>
2014-12-11 19:53:28 +0530

    bumped up version

--- app/src/main/java/com/eventshigh/nearme/app/data/EventsFetcher.java
+++ app/src/main/java/com/eventshigh/nearme/app/data/EventsFetcher.java
@@ -30,7 +30,7 @@ public class EventsFetcher extends AsyncTask<EventFetcherParam, Void, List<Event
 
     private static final String LOG_TAG = EventsFetcher.class.getSimpleName();
     private static final String API_ENDPOINT =
-            "http://apiserver.eventshigh.com:8888/api/date/CITY/DATE?sortby=popularity&limit=200&mobile=3";
+            "http://apiserver.eventshigh.com:8888/api/date/CITY/DATE?sortby=popularity&limit=300&mobile=4";
 
     public static interface EventsFetcherCallBack {
         public void OnEventsAvailable(EventFetcherParam param, List<Event> events);
