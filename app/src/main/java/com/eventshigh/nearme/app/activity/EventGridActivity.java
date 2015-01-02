12da9406ec783b9eb8e879f62334c34591c771fa
Parag Sarda <parag@eventshigh.com>
2015-01-02 16:43:04 +0530

    rename the menu file to match the activity name

--- app/src/main/java/com/eventshigh/nearme/app/activity/EventGridActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/EventGridActivity.java
@@ -59,7 +59,7 @@ public class EventGridActivity extends LocationAwareEventActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_list, menu);
+        getMenuInflater().inflate(R.menu.activity_grid, menu);
 
         // Search View.
         if (!pref.isMapsViewDefault()) {
