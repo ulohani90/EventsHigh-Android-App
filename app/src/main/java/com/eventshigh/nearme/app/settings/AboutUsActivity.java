695673ad88fa36c9099e1f2abc10a9a9adcd3fb8
Parag Sarda <parag@eventshigh.com>
2015-01-14 17:14:59 +0530

    about us page

---
+++ app/src/main/java/com/eventshigh/nearme/app/settings/AboutUsActivity.java
@@ -0,0 +1,38 @@
+package com.eventshigh.nearme.app.settings;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+import com.eventshigh.nearme.app.R;
+
+/**
+ * About Us Page.
+ */
+public class AboutUsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpTo(this, new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
