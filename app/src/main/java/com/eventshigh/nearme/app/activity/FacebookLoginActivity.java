289fd6032bcf50cb0f0894aac56fb2134af0bef1
Parag Sarda <parag@eventshigh.com>
2015-01-22 21:07:44 +0530

    facebook login fix

--- app/src/main/java/com/eventshigh/nearme/app/activity/FacebookLoginActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/FacebookLoginActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +16,7 @@ import com.facebook.Request.GraphUserCallback;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 
@@ -25,8 +27,8 @@ import java.util.Arrays;
  * we directly go to next screen passed in parameter.
  */
 public class FacebookLoginActivity extends BaseActivity {
-    private Session session;
-    LoginButton authButton;
+    private LoginButton authButton;
+    private UiLifecycleHelper uiHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +44,24 @@ public class FacebookLoginActivity extends BaseActivity {
             }
         });
 
-        session = Session.openActiveSession(this, false, callback);
-
         authButton = (LoginButton) findViewById(R.id.auth_button);
-        authButton.setReadPermissions(Arrays.asList("email"));
+        authButton.setReadPermissions(Arrays.asList("email", "public_profile"));
         authButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Session.openActiveSession(FacebookLoginActivity.this, true, callback);
+                Session session = Session.getActiveSession();
+                if (session != null && !session.isOpened() && !session.isClosed()) {
+                    session.openForRead(new Session.OpenRequest(FacebookLoginActivity.this)
+                            .setPermissions(Arrays.asList("email", "public_profile"))
+                            .setCallback(callback));
+                } else {
+                    Session.openActiveSession(FacebookLoginActivity.this, true, callback);
+                }
             }
         });
+
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
     }
 
     @Override
@@ -83,12 +93,47 @@ public class FacebookLoginActivity extends BaseActivity {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null) {
+            authButton.setSession(session);
+            if (session.isOpened()) {
+                callback.call(session, session.getState(), null);
+            }
+        }
+
+        uiHelper.onResume();
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (session != null) {
-            session.onActivityResult(this, requestCode, resultCode, data);
-        }
+        uiHelper.onActivityResult(requestCode, resultCode, data);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
 
     private void up() {
         NavUtils.navigateUpTo(this, new Intent(this, ShareAppActivity.class));
@@ -97,7 +142,6 @@ public class FacebookLoginActivity extends BaseActivity {
     private Session.StatusCallback callback = new Session.StatusCallback() {
         @Override
         public void call(Session session, SessionState state, Exception exception) {
-            FacebookLoginActivity.this.session = session;
             authButton.setSession(session);
 
             /*
