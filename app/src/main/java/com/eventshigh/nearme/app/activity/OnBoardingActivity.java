018a587ea75d307d1c91334824640849589a9602
Chandan Pitta <chandan.pitta@gmail.com>
2015-01-23 21:44:25 +0530

    Skeleton code for on-boarding activity

---
+++ app/src/main/java/com/eventshigh/nearme/app/activity/OnBoardingActivity.java
@@ -0,0 +1,15 @@
+package com.eventshigh.nearme.app.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.eventshigh.nearme.app.R;
+
+public class OnBoardingActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
