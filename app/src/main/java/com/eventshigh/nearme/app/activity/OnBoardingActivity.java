5d9a3a58a01d1c7133cd29ba6eca49a18d5cdc85
Chandan Pitta <chandan.pitta@gmail.com>
2015-01-25 21:34:35 +0530

    Documented code and added a flag to enable/disable onboarding activity. Disabled the flag for release.

--- app/src/main/java/com/eventshigh/nearme/app/activity/OnBoardingActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/OnBoardingActivity.java
@@ -12,15 +12,38 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.eventshigh.nearme.app.BuildConfig;
 import com.eventshigh.nearme.app.R;
 import com.eventshigh.nearme.app.settings.Preferences;
 import com.eventshigh.nearme.app.utils.Utils;
 
+/**
+ * Onboarding activity for first time users. This activity is shown to a user when the app is
+ * launched and keeps showing until the user skips the onboarding steps.
+ */
 public class OnBoardingActivity extends FragmentActivity implements ViewPager.OnPageChangeListener {
+    /**
+     * The view pager navigation dots shown below the view pager. A big dot indicates the step
+     * that the user is currently on when swiping through the view pager content.
+     */
     private LinearLayout dotsView;
+
+    /** The view pager which shows the onboarding steps */
     private ViewPager viewPager;
+
+    /** The last view pager position from which the user navigated. */
     private int lastPosition = 0;
+
+    /**
+     * The layout param that makes the view pager dot look big indicating the current page the user
+     * is currently on.
+     */
     private LinearLayout.LayoutParams bigDotLayoutParams;
+
+    /**
+     * The layout param the makes the view pager dots look small indicating the number of pages in
+     * the view pager.
+     */
     private LinearLayout.LayoutParams smallDotLayoutParams;
 
     @Override
@@ -28,6 +51,7 @@ public class OnBoardingActivity extends FragmentActivity implements ViewPager.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
+        // Convert dp units to pixels, because layout params uses px units
         int dp4 = Utils.dpToPx(this, 4);
         int dp8 = Utils.dpToPx(this, 8);
         int dp12 = Utils.dpToPx(this, 12);
@@ -42,11 +66,16 @@ public class OnBoardingActivity extends FragmentActivity implements ViewPager.On
         viewPager.setAdapter(new Adapter());
         viewPager.setOnPageChangeListener(this);
 
-        if (!Preferences.getInstance(this).isFirstLaunch()) {
+        // If the SHOW_ONBOARDING flag is disabled or if the user has already seen the onboarding
+        // steps, then finish this activity and start the {@link LaunchActivity}.
+        if (!BuildConfig.SHOW_ONBOARDING || !Preferences.getInstance(this).isFirstLaunch()) {
             startLaunchActivity();
         }
     }
 
+    /**
+     * Starts the {@link LaunchActivity} and finishes this activity.
+     */
     private void startLaunchActivity() {
         startActivity(new Intent(this, LaunchActivity.class));
         finish();
@@ -58,8 +87,11 @@ public class OnBoardingActivity extends FragmentActivity implements ViewPager.On
 
     @Override
     public void onPageSelected(int position) {
+        // Change the last active dot to small dot
         dotsView.getChildAt(lastPosition).setLayoutParams(smallDotLayoutParams);
+        // Change the currently active dot to big dot
         dotsView.getChildAt(position).setLayoutParams(bigDotLayoutParams);
+        // Keep track of the currently active page
         lastPosition = position;
     }
 
@@ -68,13 +100,20 @@ public class OnBoardingActivity extends FragmentActivity implements ViewPager.On
     }
 
     public void onSkipClicked(View view) {
+        // If user skipped this activity then setup a flag that {@link OnBoardingActivity} need not
+        // be shown again on the next launch.
         Preferences.getInstance(this).setPrefFirstLaunch(false);
         startLaunchActivity();
     }
 
+    /** The view pager adapter that creates the views for each onboarding step. */
     private class Adapter extends PagerAdapter {
+        /** The model that stores the resources for each onboarding step. */
         private class ViewResources {
+            // The text to be shown below the image in onboarding step
             private final int captionResId;
+
+            // The image to be shown in onboarding step
             private final int imageResId;
 
             private ViewResources(int captionResId, int imageResId) {
@@ -94,11 +133,15 @@ public class OnBoardingActivity extends FragmentActivity implements ViewPager.On
 
         public Adapter() {
             layoutInflater = getLayoutInflater();
+
+            // Add dots to indicate the number of pages in view pager
             for (int i = 0; i < viewResourceses.length; i++) {
                 View view = layoutInflater.inflate(R.layout.viewpager_dot, dotsView, false);
                 dotsView.addView(view);
                 view.setLayoutParams(smallDotLayoutParams);
             }
+
+            // Make the dot corresponding to currently active page big.
             dotsView.getChildAt(0).setLayoutParams(bigDotLayoutParams);
         }
 
