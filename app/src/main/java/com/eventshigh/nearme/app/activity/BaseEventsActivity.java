442842f25342e26e0d876f81aa55ade2f82e0c56
Parag Sarda <parag@eventshigh.com>
2015-02-12 15:35:03 +0530

    tab title: favourites

--- app/src/main/java/com/eventshigh/nearme/app/activity/BaseEventsActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/BaseEventsActivity.java
@@ -659,7 +659,7 @@ public abstract class BaseEventsActivity extends BaseActivity {
         }
 
         private boolean isFavouriteView(int position) {
-            return tabs.get(position).first == EventTab.FAVOURITE;
+            return tabs.get(position).first == EventTab.FAVOURITES;
         }
 
         @Override
