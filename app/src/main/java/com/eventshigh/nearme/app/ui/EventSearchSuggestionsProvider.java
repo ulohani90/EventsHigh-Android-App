14a3eaf458da2360e021c667a140ee07d6fe7683
Chandan Pitta <chandan.pitta@gmail.com>
2015-02-28 14:19:34 +0530

    Show event names in search autosuggest

--- app/src/main/java/com/eventshigh/nearme/app/ui/EventSearchSuggestionsProvider.java
+++ app/src/main/java/com/eventshigh/nearme/app/ui/EventSearchSuggestionsProvider.java
@@ -12,7 +12,16 @@ import android.provider.SearchRecentSuggestions;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.eventshigh.nearme.app.R;
+import com.eventshigh.nearme.app.network.VolleyHelper;
 import com.eventshigh.nearme.app.utils.StreamUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -25,8 +34,11 @@ public class EventSearchSuggestionsProvider extends SearchRecentSuggestionsProvi
     private final static String AUTHORITY = "com.eventshigh.nearme.app.EventSearchSuggestions";
     private final static int MODE = DATABASE_MODE_QUERIES;
     private final static int SUGGESTION_ID_START = 10000;
+    private final static int EVENT_ID_START = 20000;
 
     private String[] allTags;
+    private SuggestEvent[] allEvents;
+    private JsonArrayRequest request;
 
     public EventSearchSuggestionsProvider() {
         setupSuggestions(AUTHORITY, MODE);
@@ -46,6 +58,7 @@ public class EventSearchSuggestionsProvider extends SearchRecentSuggestionsProvi
         int idIndex = recentsCursor.getColumnIndex(BaseColumns._ID);
         int titleColumnIndex = recentsCursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1);
         int queryColumnIndex = recentsCursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_QUERY);
+        int iconColumnIndex = recentsCursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_ICON_1);
         int columnCount = recentsCursor.getColumnCount();
         for (int i = 0; i < allTags.length && suggestionsCursor.getCount() < 5; i++) {
             if (allTags[i].startsWith(selectionArgs[0])) {
@@ -54,6 +67,22 @@ public class EventSearchSuggestionsProvider extends SearchRecentSuggestionsProvi
                 newRow[queryColumnIndex] = allTags[i];
                 newRow[titleColumnIndex] = allTags[i];
                 suggestionsCursor.addRow(newRow);
+            }
+        }
+
+        if (allEvents == null) {
+            loadEvents();
+        } else {
+            for (int i = 0; i < allEvents.length && suggestionsCursor.getCount() < 5; i++) {
+                String suggestion = allEvents[i].name;
+                if (suggestion.contains(selectionArgs[0])) {
+                    Object[] newRow = new Object[columnCount];
+                    newRow[idIndex] = EVENT_ID_START + i;
+                    newRow[queryColumnIndex] = suggestion;
+                    newRow[titleColumnIndex] = suggestion;
+                    newRow[iconColumnIndex] = R.drawable.ic_location_on_white_36dp;
+                    suggestionsCursor.addRow(newRow);
+                }
             }
         }
 
@@ -71,6 +100,39 @@ public class EventSearchSuggestionsProvider extends SearchRecentSuggestionsProvi
         }
     }
 
+    private synchronized void loadEvents() {
+        // Request is already in flight
+        if (request != null) {
+            return;
+        }
+
+        request = new JsonArrayRequest("https://s3-ap-southeast-1.amazonaws.com/"
+                + "ehautocomplete/autocomplete_events_bangalore.json",
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray, boolean b) {
+                        allEvents = new SuggestEvent[jsonArray.length()];
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            try {
+                                allEvents[i] = SuggestEvent.parse(jsonArray.getJSONObject(i));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                synchronized (this) {
+                    // Try again
+                    request = null;
+                }
+            }
+        });
+
+        VolleyHelper.addToRequestQueue(getContext(), request);
+    }
+
     public static void saveRecentQuery(Context context, String query) {
         SearchRecentSuggestions suggestions = new SearchRecentSuggestions(context, AUTHORITY, MODE);
         suggestions.saveRecentQuery(query, null);
@@ -79,5 +141,24 @@ public class EventSearchSuggestionsProvider extends SearchRecentSuggestionsProvi
     public static void clearHistory(Context context) {
         SearchRecentSuggestions suggestions = new SearchRecentSuggestions(context, AUTHORITY, MODE);
         suggestions.clearHistory();
+    }
+
+    private static class SuggestEvent {
+        private final String id;
+        private final String city;
+        private final String type;
+        private final String name;
+
+        public SuggestEvent(String id, String city, String type, String name) {
+            this.id = id;
+            this.city = city;
+            this.type = type;
+            this.name = name;
+        }
+
+        public static SuggestEvent parse(JSONObject jsonObject) throws JSONException {
+            return new SuggestEvent(jsonObject.getString("id"), jsonObject.getString("city"),
+                    jsonObject.getString("type"), jsonObject.getString("name"));
+        }
     }
 }
