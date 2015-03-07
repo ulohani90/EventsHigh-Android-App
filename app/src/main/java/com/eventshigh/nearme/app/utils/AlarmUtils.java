c46d53d2cec449c5915dc0ce838241577e8997c6
Chandan Pitta <chandan.pitta@gmail.com>
2015-03-07 12:06:58 +0530

    Refactor notifications.

--- app/src/main/java/com/eventshigh/nearme/app/utils/AlarmUtils.java
+++ app/src/main/java/com/eventshigh/nearme/app/utils/AlarmUtils.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.os.Parcel;
 import android.text.format.DateUtils;
 
+import com.eventshigh.nearme.app.broadcast.EventNotificationIntentService;
 import com.eventshigh.nearme.app.broadcast.GcmBroadcastReceiver;
-import com.eventshigh.nearme.app.broadcast.GcmIntentService;
 import com.eventshigh.nearme.app.data.Event;
 
+/**
+ * Helper class to set and cancel alarms for an event.
+ */
 public class AlarmUtils {
     public static void setAlarm(Context context, Event event) {
         AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -18,7 +21,7 @@ public class AlarmUtils {
         event.writeToParcel(parcel, 0);
         parcel.setDataPosition(0);
         Intent intent = new Intent(context, GcmBroadcastReceiver.class);
-        intent.putExtra(GcmIntentService.BUNDLE_EVENT_KEY, parcel.marshall());
+        intent.putExtra(EventNotificationIntentService.BUNDLE_EVENT_KEY, parcel.marshall());
         PendingIntent alarmIntent = PendingIntent.getBroadcast(context,
                 event.hashCode(), intent, PendingIntent.FLAG_CANCEL_CURRENT);
         // Setup an alarm 1 day before the event
