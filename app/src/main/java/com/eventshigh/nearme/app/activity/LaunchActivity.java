23ce236c238cd3198bfd023383d39f9f9cfe5a02
Parag Sarda <parag@eventshigh.com>
2015-03-10 18:59:59 +0530

    navigation drawer is now on top on action bar

--- app/src/main/java/com/eventshigh/nearme/app/activity/LaunchActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/LaunchActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -125,6 +126,10 @@ public class LaunchActivity extends BaseActivity {
         viewSwitcher = (ViewSwitcher) findViewById(R.id.view_switcher);
         dotsView = (LinearLayout) findViewById(R.id.dots_parent);
         featuredEventsPager = (ViewPager) findViewById(R.id.featured_events_pager);
+
+        // Setup the actionbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Setup the Drawer Layout.
         drawer = (DrawerLayout) findViewById(R.id.nav_drawer);
