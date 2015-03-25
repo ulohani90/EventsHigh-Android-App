44ecdfff307218556af0b71e127a10d58cb1f426
Parag Sarda <parag@eventshigh.com>
2015-03-25 17:13:56 +0530

    minor fix

--- app/src/main/java/com/eventshigh/nearme/app/activity/LaunchActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/LaunchActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
