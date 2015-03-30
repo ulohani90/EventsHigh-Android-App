1bcf7b494799c93d3b20feaac5da933bfb245e0b
Parag Sarda <parag@eventshigh.com>
2015-03-30 17:05:45 +0530

    release 101

--- app/src/main/java/com/eventshigh/nearme/app/broadcast/UploadUserActionsService.java
+++ app/src/main/java/com/eventshigh/nearme/app/broadcast/UploadUserActionsService.java
@@ -59,8 +59,7 @@ public class UploadUserActionsService extends IntentService {
 
     private void recordUserActions(Context context, final Response.Listener<JSONObject> onSuccess,
                                    final Response.ErrorListener onFailed) {
-        Uri uri = AccountStateReporter.getApiUri(context, "mobile_app_users")
-                .appendPath("record_user_action")
+        Uri uri = AccountStateReporter.getBaseUri(context, "record_user_action")
                 .build();
         try {
             final long lastUploadTimestamp = preferences.getLong(
