01d955295f2b14ac59e396ac5450d64a624088fa
Parag Sarda <parag@eventshigh.com>
2015-04-10 23:36:47 +0530

    comment out the feedback library for now

--- app/src/main/java/com/eventshigh/nearme/app/activity/SettingsActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/SettingsActivity.java
@@ -1,16 +1,13 @@
 package com.eventshigh.nearme.app.activity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.eventshigh.nearme.app.R;
 import com.eventshigh.nearme.app.ui.EventSearchSuggestionsProvider;
-
-import io.doorbell.android.Doorbell;
 
 /**
  * A dummy activity used to handle the settings actions.
@@ -24,24 +21,25 @@ public class SettingsActivity extends BaseActivity {
         if (intent != null && intent.getAction() != null) {
             if (intent.getAction().equals("com.eventshigh.send_feedback")) {
                 reportActionToAnalytics("sendFeedback");
-//                try {
-//                    startActivity(new Intent(
-//                            Intent.ACTION_SENDTO,
-//                            Uri.parse("mailto:contact@eventshigh.com?subject=Mobile%20App%20Feedback")
-//                    ));
-//                } catch (ActivityNotFoundException e) {
-//                    // Ignore.
-//                }
-                final AlertDialog dialog = new Doorbell(
-                    this, 1119, "qXYpPYOzPQJ4CHtDFp2u0Kw2ZAXkPCPcd6cZ7LsLAj0NnJSqGKRI4Uy5Y7RoHmIK")
-                    .setPoweredByVisibility(View.GONE)
-                    .show();
-                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        finish();
-                    }
-                });
+                try {
+                    startActivity(new Intent(
+                            Intent.ACTION_SENDTO,
+                            Uri.parse("mailto:contact@eventshigh.com?subject=Mobile%20App%20Feedback")
+                    ));
+                } catch (ActivityNotFoundException e) {
+                    // Ignore.
+                }
+//                final AlertDialog dialog = new Doorbell(
+//                    this, 1119, "qXYpPYOzPQJ4CHtDFp2u0Kw2ZAXkPCPcd6cZ7LsLAj0NnJSqGKRI4Uy5Y7RoHmIK")
+//                    .setPoweredByVisibility(View.GONE)
+//                    .show();
+//                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+//                    @Override
+//                    public void onDismiss(DialogInterface dialog) {
+//                        finish();
+//                    }
+//                });
+//                return;
             }
 
             if (intent.getAction().equals("com.eventshigh.delete_query_history")) {
@@ -52,6 +50,6 @@ public class SettingsActivity extends BaseActivity {
             }
         }
 
-        //finish();
+        finish();
     }
 }
