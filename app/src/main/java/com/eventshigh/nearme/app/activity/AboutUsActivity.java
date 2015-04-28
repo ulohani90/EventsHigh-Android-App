dc70d10e3623d497aa09add3bb78325f79465e43
Parag Sarda <parag@eventshigh.com>
2015-04-28 12:34:43 +0530

    minor fix

--- app/src/main/java/com/eventshigh/nearme/app/activity/AboutUsActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/AboutUsActivity.java
@@ -1,8 +1,6 @@
 package com.eventshigh.nearme.app.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
 
 import com.eventshigh.nearme.app.R;
 
@@ -14,23 +12,5 @@ public class AboutUsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
