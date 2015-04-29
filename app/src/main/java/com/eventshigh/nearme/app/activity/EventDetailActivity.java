f700b130ab84dd69a0a07ff3856bced1e1e08b6f
Parag Sarda <parag@eventshigh.com>
2015-04-29 17:24:17 +0530

    show price in detail screen

--- app/src/main/java/com/eventshigh/nearme/app/activity/EventDetailActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/EventDetailActivity.java
@@ -447,6 +447,7 @@ public class EventDetailActivity extends BaseActivity {
         private final FrameLayout bookView;
         private final FrameLayout callView;
         private final TextView priceView;
+        private final TextView price2View;
         private final TextView offerView;
 
         private final View tagsHeaderView;
@@ -497,6 +498,7 @@ public class EventDetailActivity extends BaseActivity {
             bookView = (FrameLayout) findViewById(R.id.book_ticket);
             callView = (FrameLayout) findViewById(R.id.call);
             priceView = (TextView) findViewById(R.id.event_price);
+            price2View = (TextView) findViewById(R.id.event_price2);
             offerView = (TextView) findViewById(R.id.offer_text);
 
             tagsHeaderView = findViewById(R.id.tags_header);
@@ -578,9 +580,11 @@ public class EventDetailActivity extends BaseActivity {
             String priceString = event.getPriceString();
             if (priceString == null) {
                 priceView.setVisibility(View.GONE);
+                price2View.setText(getString(R.string.no_price));
             } else {
                 priceView.setVisibility(View.VISIBLE);
                 priceView.setText(priceString);
+                price2View.setText(priceString);
             }
 
             // Set time.
