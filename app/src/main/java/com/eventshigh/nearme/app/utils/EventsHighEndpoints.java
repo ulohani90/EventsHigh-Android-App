1978631d737341b16c7c5db32396551bf92ccfb3
Parag Sarda <parag@eventshigh.com>
2015-05-12 12:13:47 +0530

    remove unused explore api

--- app/src/main/java/com/eventshigh/nearme/app/utils/EventsHighEndpoints.java
+++ app/src/main/java/com/eventshigh/nearme/app/utils/EventsHighEndpoints.java
@@ -24,8 +24,6 @@ public class EventsHighEndpoints {
             "http://apiserver.eventshigh.com:8888/api/events/%s/%s?limit=100&mobile=1";
     private static final String API_ENDPOINT_FEATURED_FORMAT =
             "http://apiserver.eventshigh.com:8888/api/get_featured_events/%s?mobile=1";
-    private static final String API_ENDPOINT_EXPLORE_FORMAT =
-            "http://apiserver.eventshigh.com:8888/api/get_mobile_explore_screen/%s?mobile=1";
     private static final String API_ENDPOINT_EVENT_UBER_FORMAT =
             "http://apiserver.eventshigh.com:8888/api/get_event_uber_info/%s?mobile=1";
     private static final String API_EVENTS_SUGGEST_FORMAT =
@@ -89,10 +87,6 @@ public class EventsHighEndpoints {
 
     public static String getFeaturedEventsEndpoint(City city) {
         return String.format(API_ENDPOINT_FEATURED_FORMAT,  city.toString().toLowerCase());
-    }
-
-    public static String getExploreEventsEndpoint(City city) {
-        return String.format(API_ENDPOINT_EXPLORE_FORMAT, city.toString().toLowerCase());
     }
 
     public static String getApiEndpoint(EventsContext eventsContext) throws IllegalArgumentException {
