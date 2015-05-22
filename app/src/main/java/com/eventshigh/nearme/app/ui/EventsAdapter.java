bbff785a803c5732b9f52d72f6a8ed91cf213fa0
Chandan Pitta <chandan.pitta@gmail.com>
2015-05-22 21:39:19 +0530

    Revert "follow tags icon wired to implementation. also report analytics data"
    This reverts commit 8522c3575fa721ef26907576bdc11baee7ad9fe5.

--- app/src/main/java/com/eventshigh/nearme/app/ui/EventsAdapter.java
+++ app/src/main/java/com/eventshigh/nearme/app/ui/EventsAdapter.java
@@ -868,10 +868,7 @@ public class EventsAdapter extends RecyclerView.Adapter<ViewHolder> {
                     public void onClick(View v) {
                         final Account account = new Account(imageView.getContext());
                         boolean isFollowing = account.isFollowing(data.tag);
-                        isFollowing = !isFollowing;
-                        account.setIsFollowing(data.tag, isFollowing);
-                        eventsFragment.getContextActivity().reportActionToAnalytics(
-                            isFollowing ? "addFollowing" : "removeFollowing", data.tag);
+                        account.setIsFollowing(data.tag, !isFollowing);
                         setFollowView(data.tag);
                     }
                 });
