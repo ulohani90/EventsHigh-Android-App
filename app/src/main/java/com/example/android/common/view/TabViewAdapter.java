ca82743560f6fc9e12f56eb071d0d99ccb9754cb
Chandan Pitta <chandan.pitta@gmail.com>
2015-06-17 22:04:08 +0530

    remove SlidingTabLayout

--- app/src/main/java/com/example/android/common/view/TabViewAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.common.view;
-
-import android.view.View;
-import android.view.ViewGroup;
-
-public interface TabViewAdapter {
-    View getView(int position, ViewGroup parent);
-}
