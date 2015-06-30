c9df69c199c9b90027f6192be2d7cc019a82236f
Chandan Pitta <chandan.pitta@gmail.com>
2015-06-30 20:33:31 +0530

    fixed a bug that is causing the app to crash when retry button is clicked in my friends

--- app/src/main/java/com/eventshigh/nearme/app/activity/MyFriendsActivity.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/MyFriendsActivity.java
@@ -1,11 +1,14 @@
 package com.eventshigh.nearme.app.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Shows the user's friends. See {@link ContactsFragment}.
  */
 public class MyFriendsActivity extends BaseActivity {
+    private ContactsFragment contactsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +16,12 @@ public class MyFriendsActivity extends BaseActivity {
         // If not already added to the Fragment manager add it. If you don't do this a
         // new Fragment will be added every time this method is called (Such as on orientation change)
         if(savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(android.R.id.content, new ContactsFragment()).commit();
+            contactsFragment = new ContactsFragment();
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, contactsFragment).commit();
         }
+    }
+
+    public void onRetry(View view) {
+        contactsFragment.refresh(true);
     }
 }
