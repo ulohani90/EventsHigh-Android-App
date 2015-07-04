170d3167ee4123f5eecf8e5c00d04089270c5a2b
Parag Sarda <parag@eventshigh.com>
2015-07-04 15:10:20 +0530

    do not show followers on explore screen

--- app/src/main/java/com/eventshigh/nearme/app/activity/ExploreFragment.java
+++ app/src/main/java/com/eventshigh/nearme/app/activity/ExploreFragment.java
@@ -17,8 +17,6 @@ import com.eventshigh.nearme.app.data.EventsContext;
 import com.eventshigh.nearme.app.data.Locality;
 import com.eventshigh.nearme.app.network.FeaturedEventsRequest;
 import com.eventshigh.nearme.app.network.FeaturedEventsRequest.EventCollection;
-import com.eventshigh.nearme.app.network.SocialActionsRequest;
-import com.eventshigh.nearme.app.network.SocialActionsRequest.SocialActions;
 import com.eventshigh.nearme.app.network.VolleyHelper;
 import com.eventshigh.nearme.app.ui.EventsAdapter;
 import com.eventshigh.nearme.app.ui.HideActionBarOnScroll;
@@ -102,22 +100,6 @@ public class ExploreFragment extends BaseEventsFragment {
         VolleyHelper.getRequestQueue(activity).cancelAll(this);
         FeaturedEventsRequest.submit(activity, eventsContext, Priority.IMMEDIATE, this,
                 false, mFetcherCallBack, mErrorListener);
-
-        // Load social actions.
-        SocialActionsRequest.submit(activity, Priority.LOW, this, false,
-                new Listener<SocialActions>() {
-                    @Override
-                    public void onResponse(SocialActions socialActions, boolean isIntermediate) {
-                        eventsAdapter.setSocialActions(socialActions);
-                    }
-                },
-                new ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        VolleyHelper.log(activity, volleyError);
-                    }
-                }
-        );
     }
 
     private Listener<EventCollection> mFetcherCallBack = new Listener<EventCollection>() {
