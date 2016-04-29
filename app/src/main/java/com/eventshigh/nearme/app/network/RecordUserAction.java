3f86bdfaa9e6cd047e153ca67b2598824be227a3
Umesh Lohani <ulohani90@gmail.com>
2016-04-29 11:54:17 +0530

    Added lat long as 0.

--- app/src/main/java/com/eventshigh/nearme/app/network/RecordUserAction.java
+++ app/src/main/java/com/eventshigh/nearme/app/network/RecordUserAction.java
@@ -40,8 +40,8 @@ public class RecordUserAction extends JsonObjectRequest {
             actionObj.put(UserActionHelper.JSON_KEY_TIMESTAMP, Long.toString(System.currentTimeMillis()));
             actionObj.put(UserActionHelper.JSON_KEY_ACTION, action);
             actionObj.put(UserActionHelper.JSON_KEY_DATA, data);
-            actionObj.put("lat", new Account(context).getLastCity().cityBounds.getCenter().latitude);
-            actionObj.put("lon", new Account(context).getLastCity().cityBounds.getCenter().longitude);
+            actionObj.put("lat", 0);
+            actionObj.put("lon", 0);
 
             actions.put(actionObj);
 
