1d1e32ec25f30844339d4993b6e6f320eae3402c
Umesh Lohani <ulohani90@gmail.com>
2016-05-10 23:26:51 +0530

    Referrer Screens Added

---
+++ app/src/main/java/com/eventshigh/nearme/app/activity/ReferralCodeActivity.java
@@ -0,0 +1,19 @@
+package com.eventshigh.nearme.app.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by umesh on 10/05/16.
+ */
+public class ReferralCodeActivity extends BaseContextActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
